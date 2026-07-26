159e1c05ae7c4056d517b2c11c4fd401dc13166a
ThatsHoon <gagea45@gmail.com>
2026-07-26 14:17:42 +0900

    sim: actor_cmd 기반 사람 포즈 보간(idle/sit/lie/fall/recover)

--- sim/src/world_plugin.cpp
+++ sim/src/world_plugin.cpp
@@ -4,6 +4,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose.hpp>
 #include <std_msgs/msg/float64.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <algorithm>
 
 namespace happy_care_sim
 {
@@ -29,7 +31,12 @@ public:
     person_speed_pub_ = ros_node_->create_publisher<std_msgs::msg::Float64>(
       "/happy_care/person/torso_speed", 10);
 
+    actor_cmd_sub_ = ros_node_->create_subscription<std_msgs::msg::String>(
+      "/happy_care/actor_cmd", 10,
+      std::bind(&HappyCareWorldPlugin::OnActorCmd, this, std::placeholders::_1));
+
     last_update_time_ = world_->SimTime();
+    transition_start_time_ = last_update_time_;
     auto person = world_->ModelByName(person_model_name_);
     if (person) {
       last_person_pos_ = person->WorldPose().Pos();
@@ -42,6 +49,23 @@ public:
   }
 
 private:
+  static ignition::math::Pose3d IdlePose()
+  {
+    return ignition::math::Pose3d(0, 0, 0.9, 0, 0, 0);
+  }
+  static ignition::math::Pose3d SitPose()
+  {
+    return ignition::math::Pose3d(0, 0, 0.5, 0, 0, 0);
+  }
+  static ignition::math::Pose3d LiePose()
+  {
+    return ignition::math::Pose3d(0, 0, 0.15, 0, 1.5708, 0);
+  }
+  static ignition::math::Pose3d FallPose()
+  {
+    return ignition::math::Pose3d(0, 0, 0.15, 0, 1.5708, 0.7);
+  }
+
   static geometry_msgs::msg::Pose ToMsg(const ignition::math::Pose3d & p)
   {
     geometry_msgs::msg::Pose msg;
@@ -53,6 +77,32 @@ private:
     msg.orientation.z = p.Rot().Z();
     msg.orientation.w = p.Rot().W();
     return msg;
+  }
+
+  void OnActorCmd(const std_msgs::msg::String::SharedPtr msg)
+  {
+    auto person = world_->ModelByName(person_model_name_);
+    if (!person) {
+      return;
+    }
+
+    ignition::math::Pose3d next_target;
+    if (msg->data == "idle" || msg->data == "recover") {
+      next_target = IdlePose();
+    } else if (msg->data == "sit") {
+      next_target = SitPose();
+    } else if (msg->data == "lie") {
+      next_target = LiePose();
+    } else if (msg->data == "fall") {
+      next_target = FallPose();
+    } else {
+      RCLCPP_WARN(ros_node_->get_logger(), "unknown actor_cmd: %s", msg->data.c_str());
+      return;
+    }
+
+    start_pose_ = person->WorldPose();
+    target_pose_ = next_target;
+    transition_start_time_ = world_->SimTime();
   }
 
   void OnUpdate()
@@ -76,13 +126,19 @@ private:
 
     auto person = world_->ModelByName(person_model_name_);
     if (person) {
-      auto person_pose = person->WorldPose();
-      person_pose_pub_->publish(ToMsg(person_pose));
+      double t = std::min(
+        1.0, (now - transition_start_time_).Double() / transition_duration_);
+      ignition::math::Pose3d interp(
+        start_pose_.Pos() + (target_pose_.Pos() - start_pose_.Pos()) * t,
+        ignition::math::Quaterniond::Slerp(t, start_pose_.Rot(), target_pose_.Rot()));
+      person->SetWorldPose(interp);
+
+      person_pose_pub_->publish(ToMsg(interp));
 
       std_msgs::msg::Float64 speed_msg;
-      speed_msg.data = (person_pose.Pos() - last_person_pos_).Length() / dt;
+      speed_msg.data = (interp.Pos() - last_person_pos_).Length() / dt;
       person_speed_pub_->publish(speed_msg);
-      last_person_pos_ = person_pose.Pos();
+      last_person_pos_ = interp.Pos();
     }
 
     last_update_time_ = now;
@@ -101,6 +157,12 @@ private:
 
   gazebo::common::Time last_update_time_;
   ignition::math::Vector3d last_person_pos_{0, 0, 0};
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr actor_cmd_sub_;
+  ignition::math::Pose3d start_pose_{IdlePose()};
+  ignition::math::Pose3d target_pose_{IdlePose()};
+  gazebo::common::Time transition_start_time_;
+  double transition_duration_ = 1.5;
 };
 
 GZ_REGISTER_WORLD_PLUGIN(HappyCareWorldPlugin)
